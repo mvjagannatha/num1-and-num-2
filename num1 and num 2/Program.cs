28f3d1c4f52a4e7a799e51e05bb19b06dee8dba6
Admin <Admin@DESKTOP-6PQE64G>
2024-01-14 22:31:44 +0530

    Add project files.

---
+++ num1 and num 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Incrementing
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter num1");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2;
+            num2 = ++num1;
+            Console.WriteLine(num2);
+            num2 = num1++;
+            Console.WriteLine(num2);
+            num2 = num1++;
+            Console.WriteLine(num2);
+            //swapping
+            int temp;
+            temp = num1;
+            num1 = num2;
+            num2= temp;
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+        }
+    }
+}
